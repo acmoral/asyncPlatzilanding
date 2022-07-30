25af2168b8f84cc3d5ff52ee7a3331e10e5fb2db
acmoral <acmoral@unal.edu.co>
2022-07-30 17:02:13 -0500

    added the new es6 knowledge

--- src/assets/Country.js
+++ src/assets/Country.js
@@ -17,20 +17,22 @@ async function getCountry(){
     });
     const input = document.querySelector('input');
     const inputCountry = input.value;
-    const countryInfo = await fetchInfo(API,inputCountry);
+    const countryInfo_fetch = await fetchInfo(API,inputCountry);
     const Infobox = document.getElementById("Infobox");
     Infobox.textContent = '';
     //name of the country
-    const commonName = await countryInfo[0].name.common;
+    const countryInfo = await countryInfo_fetch[0];
+    const {name,currencies,capital,languages,flags,latlng,cca3} = await countryInfo;
+    const commonName = await name.common;
     const commonNameComponent = document.createElement('p')
     commonNameComponent.textContent = commonName;
     //official of the country
-    const officialName = await countryInfo[0].name.official;
-    const officialName2 = await Object.values(countryInfo[0].name.nativeName)[0].official;
+    const officialName = await name.official;
+    const officialName2 = await Object.values(name.nativeName)[0].official;
     const officialNameComponent = document.createElement('p')
     officialNameComponent.textContent =  officialName+'|'+officialName2;
     //currency
-    const currency = await Object.entries(countryInfo[0].currencies);
+    const currency = await Object.entries(currencies);
     const abbreviation = await currency[0][0];
     console.log(abbreviation);
     const currencyName = await currency[0][1].name;
@@ -39,34 +41,32 @@ async function getCountry(){
     const currencySymbol = await currency[0][1].symbol;
     moneyComponent.textContent = `${abbreviation} | ${currencyName} | ${currencySymbol}`;
     //capital
-    const capital = await countryInfo[0].capital[0];
     const capitalComponent = document.createElement('p')
     capitalComponent.textContent = capital;
     //language
-    const languages = await Object.entries(countryInfo[0].languages);
+    const language_entries = await Object.entries(languages);
     const languagediv = document.createElement('div');
     console.log(languages);
-    for(let language of languages ){
+    for(let language of language_entries ){
         const textComponent = document.createElement('p')
         textComponent.textContent = `${language[0]}  |  ${language[1]}`;
         languagediv.append(textComponent);
     }
     // flag
     const imageContainer  = document.createElement('img');
-    const flagElement = await countryInfo[0].flags.png;
+    const flagElement = await flags.png;
     imageContainer.src =flagElement;
     imageContainer.alt = "flag";
     imageContainer.width = "100";
     Infobox.append(commonNameComponent,officialNameComponent,moneyComponent,capitalComponent,
         languagediv,imageContainer);
     //recenter map 
-    const latlon = await countryInfo[0].latlng;
-    var latlng = new google.maps.LatLng(latlon[0], latlon[1]);
-    await map.setCenter(latlng);
+    var latlon = new google.maps.LatLng(latlng[0], latlng[1]);
+    await map.setCenter(latlon);
     await map.setZoom(5);
 
     // hoping to get the geoJson
-    const countryCode = await  countryInfo[0].cca3
+    const countryCode = await  cca3;
     const path = await `${JsonAPi}${countryCode}.geojson`;
     map.data.loadGeoJson(path);
     map.data.setStyle({
